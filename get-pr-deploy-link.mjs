45a7d22650609306e89c85da2b5ef489e952c9a8
Ernst-D <ernst.dzerevanko@gmail.com>
2022-08-26 15:22:23 +0400

    init solution

---
+++ get-pr-deploy-link.mjs
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+const token = process.env.GH_TOKEN
+const prNumber = process.env.PR_NUMBER
+const repository = process.env.REPOSITORY
+
+const apiUrl = () => `https://api.github.com/repos/${repository}`
+
+const getPrSha = async (prNumber, ghToken) => {
+  const res = await axios.get(`${apiUrl()}/pulls/${prNumber}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${ghToken}`,
+    },
+  })
+  return res.data.head.sha
+}
+
+const getPrDeployLink = async (sha,ghToken) => {
+  const res = await axios
+  .get(`${apiUrl()}/deployments?per_page=100`, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${ghToken}`,
+    },
+  })
+  return await res.data
+//   .map(d=>d)
+  .find(deploy => deploy.sha == sha)
+}
+
+const sha = await getPrSha(prNumber,token)
+console.log(sha)
+const prDeployLink = await getPrDeployLink(sha,token)
+console.log(prDeployLink
+    // .map(d=> d.payload)
+    .payload.web_url
+    );
+
+// console.log(process.env.PR_NUMBER)
+
