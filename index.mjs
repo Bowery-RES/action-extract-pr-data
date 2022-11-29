b238c41705fb48642954bf9f4b028d31d134d31b
Ernst-D <ernst.dzerevanko@gmail.com>
2022-11-29 12:00:55 +0400

    [QA-7369] added undefined checks

--- index.mjs
+++ index.mjs
@@ -1,10 +1,14 @@
 import { setOutput, getInput, setFailed } from '@actions/core';
 import { getOctokit } from '@actions/github';
 
+/**
+ * This object is intended to store values of env variables for localhost debug.
+ * If env var `GH_DEBUG='1'` - inputs for actions will be evaluated from environment but not from Action's input
+ */
 const inputs = {
-  token: "gh_token",
-  prNumber: "pr_number",
-  repository: "repository"
+  token: "GH_TOKEN",
+  prNumber: "PR",
+  repository: "REPO"
 }
 const evalInput = (inputName) => {
   return process.env.GH_DEBUG == 1 ? process.env[inputName] : getInput(inputName) 
@@ -23,29 +27,36 @@ try {
   let comments = await octokit.request(`GET ${commentsUrl.pathname}`)
 
   if(isCompPlexLabel){
-    console.log("find the commentary and output the link");
-    let commentCompplexDeploy = comments.data.find(
-      comment => comment.body.includes("CompPlex review app")
+    let commentCompplexDeploy = isUndefined(
+      comments.data.find(
+        comment => comment.body.includes("CompPlex review app")
+      ), "Failed to find comment with Comp-plex PR deploy link"
     );
     let linkCompplexDeploy = new URL(
       commentCompplexDeploy.body.split(" ").find(val=> val.startsWith('https://compplex-client-review'))
     ); 
-    console.log(linkCompplexDeploy.origin);
+    console.log(`Comp-plex PR deploy link: ${linkCompplexDeploy.origin}`);
     setOutput("compplex_link", linkCompplexDeploy.origin);
   }
-
   if(isWebAppLabel){
-    console.log("find the comment and output the link");
-    let commentWebAppDeploy = comments.data.find(
-      comment => comment.body.includes("Webapp review app")
+    let commentWebAppDeploy = isUndefined(
+      comments.data.find(
+        comment => comment.body.includes("Webapp review app")
+        ), "Failed to find comment with WebApp PR deploy link"
     );
     let linkWebAppDeploy = new URL(
       commentWebAppDeploy.body.split(" ").find(val=> val.startsWith("https://webapp"))
     ); 
-
-    console.log(linkWebAppDeploy.origin);
+    console.log(`WebApp PR deploy link: ${linkWebAppDeploy.origin}`);
     setOutput("webapp_link", linkWebAppDeploy.origin);
   }
 } catch (error) {
   setFailed(error.message);
+}
+
+function isUndefined(value, message = "Value is undefined."){
+  if(typeof value === 'undefined'){
+    throw new Error(message)
+  }
+  return value;
 }
